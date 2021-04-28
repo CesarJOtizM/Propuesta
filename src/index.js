bdf13c7ed11e3e2abcc5560af9e3856835fee406
CesarJOritzM <cesarjavierortizmontero@gmail.com>
2021-04-28 01:04:14 -0500

    initial config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import './Assets/Styles/index.scss';
+import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
